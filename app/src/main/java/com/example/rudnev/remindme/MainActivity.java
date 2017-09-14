4dcd0bc51e272bd42812be4d351092b6b3399b65
Dootsen <Dootsen@mail.ru>
2017-09-14 15:30:02 +0400

    edit navigation view

--- app/src/main/java/com/example/rudnev/remindme/MainActivity.java
+++ app/src/main/java/com/example/rudnev/remindme/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.rudnev.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -10,12 +10,13 @@ import android.view.MenuItem;
  * Created by rudnev on 12.09.2017.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
